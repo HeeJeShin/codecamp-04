e425905ba7d81e816161b12e9ac16752f0b7f9ca
heeje0910 <shjs6711@gmail.com>
2021-12-21 13:41:58 +0900

    1

--- class_build/pages/market/[useditemId]/index.js
+++ class_build/pages/market/[useditemId]/index.js
@@ -1,20 +1,14 @@
 import Head from "next/head";
 import { gql, request } from "graphql-request";
 
-export default function BoardsPage() {
+export default function BoardsPage(props) {
   return (
     <>
       <Head>
         <meta property="og:title" content="나만의 사이트 게시판!!!" />
         {/* <meta property="og:url" content="http://heeje.site" /> */}
-        <meta
-          property="og:image"
-          content={props.fetchUseditem.images[0]}
-        />
-        <meta
-          property="og:description"
-          content={props.fetchUseditem.remarks}
-        />
+        <meta property="og:image" content={props.fetchUseditem.images[0]} />
+        <meta property="og:description" content={props.fetchUseditem.remarks} />
       </Head>
       <div>안녕하세요 상품페이지입니다.</div>
     </>
@@ -31,9 +25,9 @@ const FETCH_USEDITEM = gql`
   }
 `;
 
-export const getSeverSideProps = async (context) => {
+export const getServerSideProps = async (context) => {
   const result = await request(
-    "https://bakend4.codebootcamp.co.kr/graphql",
+    "https://backend4.codebootcamp.co.kr/graphql",
     FETCH_USEDITEM,
     {
       useditemId: context.query.useditemId,
@@ -41,12 +35,13 @@ export const getSeverSideProps = async (context) => {
   );
 
   return {
-      props: {
-          fetchUseditem: {
-              name: result.fetchUseditem.name,
-              remarks: result.fetchUseditem.remarks,
-              images: result.fetchUseditem.images[0]
-          },
-      }
+    props: {
+      fetchUseditem: {
+        name: result.fetchUseditem.name,
+        remarks: result.fetchUseditem.remarks,
+        images: result.fetchUseditem.images[0],
+      },
+    },
   };
 };
+``;
