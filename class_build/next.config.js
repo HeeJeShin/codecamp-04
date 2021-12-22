f748b9829b6a7b0ff28a44ca6258657d57de36cc
heeje0910 <shjs6711@gmail.com>
2021-12-22 13:33:36 +0900

    배포

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "heeje",
+  exportPathMap: () => ({
+    '/':{page: "/"},
+    "/boards": { page: "/boards"},
+    "/404": { page: "/404" },
+  }),
 }
