2213948559bfb60d24f47264b85db34b1da83a7a
heeje0910 <shjs6711@gmail.com>
2021-12-23 16:33:09 +0900

    Today reactnative

---
+++ freeboard_mobile/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { WebView} from 'react-native-webview'
+
+
+
+
+const App: () => Node = () => {
+  
+  return <WebView source={{uri: 'https://heeje.site'}} />;
+};
+
+
+export default App;
