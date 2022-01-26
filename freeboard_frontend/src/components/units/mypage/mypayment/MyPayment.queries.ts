99bb5566d3d732453acf721756f262b1166b8e3c
heeje0910 <shjs6711@gmail.com>
2022-01-26 14:47:32 +0900

    [220126][mypayment container 작업]

--- freeboard_frontend/src/components/units/mypage/mypayment/MyPayment.queries.ts
+++ freeboard_frontend/src/components/units/mypage/mypayment/MyPayment.queries.ts
@@ -1,1 +1,20 @@
 import { gql } from "@apollo/client";
+
+export const CREATE_POINT_TRANSACTION_OF_LOADING = gql`
+  mutation createPointTransactionOfLoading($impUid: ID!) {
+    createPointTransactionOfLoading(impUid: $impUid) {
+      _id
+      impUid
+      amount
+      balance
+      status
+      statusDetail
+      useditem {
+        name
+        price
+        remarks
+        contents
+      }
+    }
+  }
+`;
