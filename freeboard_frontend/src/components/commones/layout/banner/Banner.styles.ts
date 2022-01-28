e24d6f2353f43840c33718d98e4d4d899294e6dc
heeje0910 <shjs6711@gmail.com>
2022-01-28 22:50:32 +0900

    220128[Banner] 반응형 정리

--- freeboard_frontend/src/components/commones/layout/banner/Banner.styles.ts
+++ freeboard_frontend/src/components/commones/layout/banner/Banner.styles.ts
@@ -3,10 +3,19 @@ import styled from "@emotion/styled";
 export const BannerSection = styled.section`
   width: 70%;
   height: 500px;
-  background-color: #edceba;
+  /* background-color: #edceba; */
   overflow: hidden;
   margin-left: 300px;
-  margin-top: 100px;
+
+  @media (min-width: 768px) and (max-width: 1199px) {
+    width: 70%;
+    margin-left: 150px;
+  }
+
+  @media (max-width: 767px) {
+    width: 70%;
+    margin: 0 auto;
+  }
 `;
 
 export const MyImage = styled.img`
@@ -17,23 +26,15 @@ export const MyImage = styled.img`
 
 export const WidthWrapper = styled.div`
   display: flex;
+  /* flex-direction: center; */
   justify-content: center;
 `;
 
 export const Wrapper = styled.div`
-  /* padding: 30px; */
+  padding: 30px;
 `;
 
 export const BannerText = styled.div`
   font-size: 48px;
   text-align: center;
 `;
-
-//이미지가 png.
-
-// #b7c5c8; //약간 회파랑?
-
-//#b8b8b0 //약간 ㅊ회색?
-// #edceba  // 약간 주황
-// #f3bac0   //약간핑크
-// #edeae3    //약한 흰색회색
